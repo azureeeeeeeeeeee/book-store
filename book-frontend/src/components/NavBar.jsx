9d0b6a1e447f20527bc753db0704ea0064af3ef8
rionando <rionandosoeksin@gmail.com>
2024-06-18 22:53:48 +0800

    fixing little styling

--- book-frontend/src/components/NavBar.jsx
+++ book-frontend/src/components/NavBar.jsx
@@ -8,13 +8,16 @@ import {
   MenuItem,
 } from "@chakra-ui/react";
 import { FaShoppingCart, FaRegUser, FaSignOutAlt } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <nav className="flex items-center justify-between px-16 py-8">
+    <nav className="flex items-center justify-between px-16 py-4">
       <div className="flex-none">
         <h1>
-          <strong>Bookstore</strong>
+          <Link to={"/"}>
+            <strong>Bookstore</strong>
+          </Link>
         </h1>
       </div>
       <div className="max-w-18">
@@ -26,18 +29,24 @@ const NavBar = () => {
             <Avatar name="username" src="https://bit.ly/dan-abramov" />
           </MenuButton>
           <MenuList>
-            <MenuItem>
-              <FaShoppingCart className="mr-4" />
-              Cart
-            </MenuItem>
-            <MenuItem>
-              <FaRegUser className="mr-4" />
-              Profile
-            </MenuItem>
-            <MenuItem>
-              <FaSignOutAlt className="mr-4" />
-              Logout
-            </MenuItem>
+            <Link>
+              <MenuItem>
+                <FaShoppingCart className="mr-4" />
+                Cart
+              </MenuItem>
+            </Link>
+            <Link>
+              <MenuItem>
+                <FaRegUser className="mr-4" />
+                Profile
+              </MenuItem>
+            </Link>
+            <Link>
+              <MenuItem>
+                <FaSignOutAlt className="mr-4" />
+                Logout
+              </MenuItem>
+            </Link>
           </MenuList>
         </Menu>
       </div>
--- book-frontend/src/layouts/MainLayout.jsx
+++ book-frontend/src/layouts/MainLayout.jsx
@@ -7,6 +7,7 @@ const MainLayout = () => {
   return (
     <>
       <NavBar />
+      <hr />
       <Outlet />
       <ToastContainer />
     </>
