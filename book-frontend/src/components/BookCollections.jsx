105e1b3551f5960c15cd884a12d7708ee76a8593
rionando <rionandosoeksin@gmail.com>
2024-06-18 23:21:08 +0800

    fixing styling in the bookcollections

--- book-frontend/src/components/BookCollections.jsx
+++ book-frontend/src/components/BookCollections.jsx
@@ -3,9 +3,9 @@ import BookCard from "./BookCard";
 
 const BookCollections = () => {
   return (
-    <section>
-      <BookCard coverUrl={"../src/assets/cover-1.webp"} />
-      <BookCard coverUrl={"../src/assets/cover-2.jpg"} />
+    <section className="grid grid-cols-2 mx-24 my-20 place-items-center">
+      <BookCard coverUrl={"./cover/cover-1.webp"} />
+      <BookCard coverUrl={"./cover/cover-2.jpg"} />
     </section>
   );
 };
