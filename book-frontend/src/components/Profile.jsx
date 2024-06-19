a5bf448c2b0f6ede4a18580dd1e6cd360aede0bf
rionando <rionandosoeksin@gmail.com>
2024-06-19 20:01:25 +0800

    fix style

--- book-frontend/src/components/Profile.jsx
+++ book-frontend/src/components/Profile.jsx
@@ -1,11 +1,4 @@
-import {
-  Avatar,
-  Box,
-  Text,
-  Heading,
-  Button,
-  SimpleGrid,
-} from "@chakra-ui/react";
+import { Avatar, Box, Heading, Button, SimpleGrid } from "@chakra-ui/react";
 import BookCard from "./BookCard";
 
 const Profile = ({ imgSrc }) => {
@@ -13,17 +6,20 @@ const Profile = ({ imgSrc }) => {
     <Box className="mt-6">
       <Avatar size="2xl" src={imgSrc} />
       <Heading>Nama Lengkap - @username</Heading>
-      <Box className="">
+      <Box className="mt-4">
         <Heading size="base mx-auto">Recently Purchased :</Heading>
         <SimpleGrid
           minChildWidth="300px"
           spacing={10}
-          className="place-items-center mx-24 my-20"
+          className="place-items-center mt-24 my-20"
         >
           <BookCard coverUrl={"./cover/cover-1.webp"} />
           <BookCard coverUrl={"./cover/cover-2.jpg"} />
           <BookCard coverUrl={"./cover/cover-1.webp"} />
         </SimpleGrid>
+        <Button colorScheme="blue" className="mb-6">
+          Edit Profile
+        </Button>
       </Box>
     </Box>
   );
