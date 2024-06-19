8e916b907020a89aca29eaeec788e947f42a3b24
rionando <rionandosoeksin@gmail.com>
2024-06-19 17:08:57 +0800

    adding landing page for the register

--- book-frontend/src/components/Register.jsx
+++ book-frontend/src/components/Register.jsx
@@ -1,7 +1,42 @@
 // import React from "react";
+import { Box, Text, Heading, SimpleGrid, Button } from "@chakra-ui/react";
+import { FaRegUser, FaBuilding } from "react-icons/fa";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <>
+      <Box borderRadius="l" className="flex flex-col gap-4 w-96 mx-auto my-6">
+        <Heading as="h2" size="3xl" noOfLines={1}>
+          BookStore
+        </Heading>
+        <SimpleGrid
+          minChildWidth="200px"
+          spacing={4}
+          className="place-items-center mx-24 my-20"
+        >
+          <Box borderRadius="lg" borderWidth="1px" className="p-4 min-w-10">
+            <FaRegUser className="mx-auto text-5xl" />
+            <Heading as="h3" size="xl" className="mb-4">
+              Customer
+            </Heading>
+            <Text>Are you a customer and just want to buy book ?</Text>
+            <Button colorScheme="blue">Register as a Customer</Button>
+          </Box>
+          <Text>
+            <strong>OR</strong>
+          </Text>
+          <Box borderRadius="lg" borderWidth="1px" className="p-4">
+            <FaBuilding className="mx-auto text-5xl" />
+            <Heading as="h3" size="xl" className="mb-4">
+              Publisher
+            </Heading>
+            <Text>Are you a Publisher and want to publish your book ?</Text>
+            <Button colorScheme="blue">Register as a Publisher</Button>
+          </Box>
+        </SimpleGrid>
+      </Box>
+    </>
+  );
 };
 
 export default Register;
