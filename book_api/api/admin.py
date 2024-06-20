aabd9501a818564a9aca54cb6ca5031d80ef640e
rionando <rionandosoeksin@gmail.com>
2024-06-21 02:02:38 +0800

    adding simple database models

--- book_api/api/admin.py
+++ book_api/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Publisher, Profile, Book
 
 # Register your models here.
+admin.site.register(Publisher)
+admin.site.register(Profile)
+admin.site.register(Book)
--- book_api/api/models.py
+++ book_api/api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+class Publisher(models.Model):
+    name = models.CharField(max_length=150)
+    email = models.EmailField()
+    # picture = models.ImageField()
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    description = models.TextField(max_length=100)
+    publisher = models.ForeignKey(Publisher, null=True, on_delete=models.SET_NULL)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    fullname = models.CharField(max_length=150)
