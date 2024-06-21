0450237335658ff0aea9e035bd5ef73d0305b46a
rionando <rionandosoeksin@gmail.com>
2024-06-21 18:04:14 +0800

    adding serializers

---
+++ book_api/api/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import Publisher, Book, Profile
+from django.contrib.auth.models import User
+
+class PublisherSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=150)
+    email = serializers.EmailField()
+
+
+class BookSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(max_length=200)
+    author = serializers.CharField(max_length=100)
+    publisher = serializers.PrimaryKeyRelatedField(queryset=Publisher.objects.all(), allow_null=True)
+
+
+class ProfileSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), allow_null=True)
+    fullname = serializers.CharField(max_length=150)
