0aa38cadabe578c365687a1656a9c3ffb7bf769e
rionando <rionandosoeksin@gmail.com>
2024-06-21 22:30:49 +0800

    handling login from the frontend

--- book-frontend/src/components/Login.jsx
+++ book-frontend/src/components/Login.jsx
@@ -7,23 +7,56 @@ import {
   Heading,
 } from "@chakra-ui/react";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const Login = () => {
   const [show, setShow] = useState(false);
   const handleClick = () => setShow(!show);
+
+  const navigate = useNavigate();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    const res = await fetch("http://localhost:8000/api/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    });
+
+    if (res.ok) {
+      toast.success("Login Success");
+      navigate("/");
+    } else {
+      toast.error("Login Failed");
+      console.log("Login Failed");
+    }
+  };
 
   return (
     <Box borderRadius="l" className="flex flex-col gap-4 w-96 mx-auto my-6">
       <Heading as="h2" size="3xl" noOfLines={1}>
         BookStore
       </Heading>
-      <form action="" method="POST">
-        <Input placeholder="Enter Username" />
+      <form onSubmit={handleLogin} action="" method="POST">
+        <Input
+          placeholder="Enter Username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
         <InputGroup className="my-4" size="md">
           <Input
             pr="4.5rem"
             type={show ? "text" : "password"}
             placeholder={show ? "Enter Password" : "********"}
+            onChange={(e) => setPassword(e.target.value)}
           />
           <InputRightElement width="4.5rem">
             <Button h="1.75rem" size="sm" onClick={handleClick}>
@@ -31,7 +64,9 @@ const Login = () => {
             </Button>
           </InputRightElement>
         </InputGroup>
-        <Button colorScheme="blue">Login</Button>
+        <Button type="submit" colorScheme="blue">
+          Login
+        </Button>
       </form>
     </Box>
   );
