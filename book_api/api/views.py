2ca71a2f6cc8c170710913cf85e27bcd5dda681c
rionando <rionandosoeksin@gmail.com>
2024-06-21 18:24:34 +0800

    adding base api route

--- book_api/api/views.py
+++ book_api/api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import PublisherSerializer, BookSerializer, ProfileSerializer
+from django.contrib.auth import authenticate, login, logout
+from django.db.models import Q
+from .models import Publisher, Profile, Book
+from django.contrib.auth.models import User
 
 # Create your views here.
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        'POST /api/auth/login',
+        'POST /api/auth/register',
+        'POST /api/auth/login',
+    ]
+    return Response(routes)
